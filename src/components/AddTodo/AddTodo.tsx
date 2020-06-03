86d277d5078598324f64b2b40e6c30c28ac5cb60
chanchaldabriya <chanchaldabriya@gmail.com>
2020-06-03 23:42:08 +0530

    Initial Commit creating basic skeleton of App bootstrapped using create-react-app with 'typescript' template.

---
+++ src/components/AddTodo/AddTodo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddTodo() {
+    return (
+      <div className="AddTodo">
+        AddTodo
+      </div>
+    );
+}
+
+export default AddTodo;
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from '../Todo/Todo';
+
+function TodoList() {
+    return (
+      <div className="TodoList">
+        TodoList
+        <Todo />
+      </div>
+    );
+}
+
+export default TodoList;
